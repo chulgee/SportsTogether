f8ec378563f0535629cd4354df5d0a74aa52c76b
chulgee <chulchoice@naver.com>
2016-11-03 12:47:21 +0900

    1

--- app/src/main/java/com/iron/dragon/sportstogether/adapter/MyAdapter.java
+++ app/src/main/java/com/iron/dragon/sportstogether/adapter/MyAdapter.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by chulchoice on 2016-10-12.
+ * Created by chulchoice on 2016-10-12.d
  */
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
     private List<Sports> mDataset;
