1ba633fd7a026fbf31787e32b2cbdb786fb4e5c0
seungyong <lsy4630@gmail.com>
2016-11-16 17:23:53 +0900

    add xml layout

--- app/src/main/java/com/iron/dragon/sportstogether/LoginActivity.java
+++ app/src/main/java/com/iron/dragon/sportstogether/LoginActivity.java
@@ -22,7 +22,7 @@ public class LoginActivity extends ProfileActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_profile);
         InitLayout();
 
     }
--- app/src/main/java/com/iron/dragon/sportstogether/retrofit/GitHubService.java
+++ app/src/main/java/com/iron/dragon/sportstogether/retrofit/GitHubService.java
@@ -1,17 +1,10 @@
 package com.iron.dragon.sportstogether.retrofit;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.util.List;
-
 import okhttp3.OkHttpClient;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.Body;
-import retrofit2.http.Field;
-import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 /**
