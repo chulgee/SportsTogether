07899a707ec1e2efb8198cc919f90d11da097c15
seungyong <lsy4630@gmail.com>
2016-11-16 15:17:36 +0900

    test

--- app/src/main/java/com/iron/dragon/sportstogether/MainActivity.java
+++ app/src/main/java/com/iron/dragon/sportstogether/MainActivity.java
@@ -1,22 +1,13 @@
 package com.iron.dragon.sportstogether;
 
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
 
 import com.iron.dragon.sportstogether.abs.Factory;
-import com.iron.dragon.sportstogether.abs.Sports;
 import com.iron.dragon.sportstogether.adapter.MyAdapter;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         mList = (RecyclerView)findViewById(R.id.list);
