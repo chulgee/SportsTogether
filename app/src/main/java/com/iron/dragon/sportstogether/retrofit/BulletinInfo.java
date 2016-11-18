81302b704bab56158813ebbeca5a4731d85ae1d2
chulchoice <chulchoicezee@gmail.com>
2016-11-19 01:29:31 +0900

    chulgee1

--- app/src/main/java/com/iron/dragon/sportstogether/retrofit/BulletinInfo.java
+++ app/src/main/java/com/iron/dragon/sportstogether/retrofit/BulletinInfo.java
@@ -1,52 +1,30 @@
 package com.iron.dragon.sportstogether.retrofit;
 
+import android.net.Uri;
+
 /**
  * Created by chulchoice on 2016-11-18.
  */
 public class BulletinInfo {
 
-    String username;
-    private int locationid;
-    private int sportsid;
-    private String comment;
-    private String date;////tes//zz
+    int locationid;
+    int sportsid;
+    int comment;
+    String date;////tes//zz
 
-    //
     BulletinInfo(Builder builder){
-        username = builder.username;
         locationid = builder.locationid;
         sportsid = builder.sportsid;
         comment = builder.comment;
         date = builder.date;
     }
-    public String getUsername() {
-        return username;
-    }
-
-    public int getLocationid() {
-        return locationid;
-    }
-
-    public int getSportsid() {
-        return sportsid;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public String getDate() {
-        return date;
-    }
 
     public class Builder{
-        String username;
         int locationid;
         int sportsid;
-        String comment;
+        int comment;
         String date;
 
-        public void setUsername(String username) {this.username = username;}
         public void setLocationid(int locationid) {
             this.locationid = locationid;
         }
@@ -55,7 +33,7 @@ public class BulletinInfo {
             this.sportsid = sportsid;
         }
 
-        public void setComment(String comment) {
+        public void setComment(int comment) {
             this.comment = comment;
         }
 
@@ -67,7 +45,4 @@ public class BulletinInfo {
             this.date = date;
         }
     }
-
-
 }
-//////
