cb624ceb9711fb39a7a2ff5595c9af8a799ea631
seungyong <lsy4630@gmail.com>
2016-11-18 19:40:42 +0900

    delete 741

--- app/src/main/java/com/iron/dragon/sportstogether/ConcreteSports.java
+++ app/src/main/java/com/iron/dragon/sportstogether/ConcreteSports.java
@@ -9,7 +9,8 @@ public class ConcreteSports extends Sports {
 
     protected int id;
     protected String name;
-//
+
+
     ConcreteSports(String name, int id){
         this.name = name;
         this.id = id;
