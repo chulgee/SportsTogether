7b89f3fc962d3721df1074776d8ba674a1be5cb2
chulchoicezee <chulchoicezee@gmail.com>
2016-12-05 15:29:04 +0900

    qw

--- app/src/main/java/com/iron/dragon/sportstogether/util/Const.java
+++ app/src/main/java/com/iron/dragon/sportstogether/util/Const.java
@@ -4,6 +4,13 @@ package com.iron.dragon.sportstogether.util;
  * Created by chulchoice on 2016-11-18.
  */
 public class Const {
+    // server 설정
+    //public static final String MAIN_URL = "http://10.138.19.140:9000"; // for local test
+
+    //public static final String MAIN_URL = "http://192.168.0.6:9000"; // for local test
+    //public static final String MAIN_URL = "http://172.20.12.128:9000"; // for local test
+    //public static final String MAIN_URL = "http://172.30.1.6:9000"; // for local test
+    public static final String MAIN_URL = "http://ec2-52-78-226-5.ap-northeast-2.compute.amazonaws.com:9000"; // for aws
 
     public enum SPORTS{
         BADMINTON,
