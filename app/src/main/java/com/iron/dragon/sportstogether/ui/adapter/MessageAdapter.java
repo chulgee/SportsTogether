2f30a8385a5bfe05d85fd7ad4f5d35cbb8b9e0dc
chulchoice <chulchoicezee@gmail.com>
2016-12-22 22:31:30 +0900

    add avatar

--- app/src/main/java/com/iron/dragon/sportstogether/ui/adapter/MessageAdapter.java
+++ app/src/main/java/com/iron/dragon/sportstogether/ui/adapter/MessageAdapter.java
@@ -12,9 +12,13 @@ import android.widget.TextView;
 import com.iron.dragon.sportstogether.R;
 import com.iron.dragon.sportstogether.data.bean.Message;
 import com.iron.dragon.sportstogether.util.Util;
+import com.orhanobut.logger.Logger;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by chulchoice on 2016-11-28.
@@ -28,8 +32,10 @@ public class MessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
     private static final String TAG = "MessageAdapter";
     private List<Message> mMessages;
     RecyclerView rView;
+    Context context;
 
     public MessageAdapter(Context context, List<Message> messages) {
+        this.context = context;
         if(messages == null){
             mMessages = new ArrayList<Message>();
         }else
@@ -55,20 +61,34 @@ public class MessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
         Message message = mMessages.get(position);
         if(type == Message.TYPE_CHAT_MESSAGE){
             ViewHolder vh = (ViewHolder)holder;
+            String url = "http://ec2-52-78-226-5.ap-northeast-2.compute.amazonaws.com:9000/upload_profile?filename=" + message.getImage();
+            Logger.d("url = " + url);
             if(message.getMsgType() == Message.PARAM_MSG_OUT){
                 vh.rl1.setVisibility(View.VISIBLE);
                 vh.tv1.setText(message.getSender());
                 vh.tv2.setText(message.getMessage());
                 vh.tv5.setText(Util.getStringTime(message.getDate()));
                 vh.rl2.setVisibility(View.GONE);
-
+                if(message.getImage() != null){
+                    Picasso.with(context).load(url).resize(50, 50)
+                            .centerCrop()
+                            .into(vh.civ1);
+                }else{
+                    vh.civ1.setImageResource(R.drawable.buddy);
+                }
             }else{
                 vh.rl2.setVisibility(View.VISIBLE);
                 vh.tv3.setText(message.getSender());
                 vh.tv4.setText(message.getMessage());
                 vh.tv6.setText(Util.getStringTime(message.getDate()));
                 vh.rl1.setVisibility(View.GONE);
+                if(message.getImage() != null){
+                    Picasso.with(context).load(url).resize(50, 50)
+                            .centerCrop()
+                            .into(vh.civ1);
+                }
             }
+
         }else{
             ViewHolder_action vh = (ViewHolder_action) holder;
             vh.tv1.setText(message.getMessage());
@@ -92,6 +112,7 @@ public class MessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
 
     public class ViewHolder extends RecyclerView.ViewHolder{
         TextView tv1, tv2, tv3, tv4, tv5, tv6;
+        CircleImageView civ1, civ2;
         RelativeLayout rl1, rl2;
         public ViewHolder(View itemView) {
             super(itemView);
@@ -101,6 +122,8 @@ public class MessageAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
             tv4 = (TextView)itemView.findViewById(R.id.textView4);
             tv5 = (TextView)itemView.findViewById(R.id.textView5);
             tv6 = (TextView)itemView.findViewById(R.id.textView6);
+            civ1 = (CircleImageView)itemView.findViewById(R.id.imageView);
+            civ2 = (CircleImageView)itemView.findViewById(R.id.imageView2);
             rl1 = (RelativeLayout)itemView.findViewById(R.id.layout1);
             rl2 = (RelativeLayout)itemView.findViewById(R.id.layout2);
         }
