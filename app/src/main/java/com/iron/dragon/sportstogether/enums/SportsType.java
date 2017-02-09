abef2a1872face1e4b426faea4b4d64b3f779b5f
chulchoice <chulchoicezee@gmail.com>
2017-02-10 03:17:38 +0900

    modify main

--- app/src/main/java/com/iron/dragon/sportstogether/enums/SportsType.java
+++ app/src/main/java/com/iron/dragon/sportstogether/enums/SportsType.java
@@ -8,21 +8,30 @@ import com.iron.dragon.sportstogether.R;
 
 public enum SportsType {
 
-    Badminton(0, R.string.badminton, R.drawable.badminton_166415_640),
-    Tennis(1, R.string.tennis, R.drawable.tennis_934841_640),
-    Table_tennis(2, R.string.table_tennis, R.drawable.table_tennis_1208377_640),
-    Soccer(3, R.string.soccer, R.drawable.soccer_ball_1285164_640),
-    Baseball(4, R.string.baseball, R.drawable.hit_1407826_640),
-    Basketball(5, R.string.basketball, R.drawable.basketball_1081882_640);
+/*    Badminton(0, R.string.badminton, R.drawable.badminton, R.color.sports_1),
+    Tennis(1, R.string.tennis, R.drawable.tennis, R.color.sports_2),
+    Table_tennis(2, R.string.table_tennis, R.drawable.table_tennis, R.color.sports_3),
+    Soccer(3, R.string.soccer, R.drawable.soccer, R.color.sports_4),
+    Baseball(4, R.string.baseball, R.drawable.baseball, R.color.sports_5),
+    Basketball(5, R.string.basketball, R.drawable.basketball, R.color.sports_6);*/
+
+    Badminton(0, R.string.badminton, R.color.sports_1, R.drawable.badminton_166415_640),
+    Tennis(1, R.string.tennis, R.color.sports_2, R.drawable.tennis_934841_640),
+    Table_tennis(2, R.string.table_tennis, R.color.sports_3, R.drawable.table_tennis_1208377_640),
+    Soccer(3, R.string.soccer, R.color.sports_4, R.drawable.soccer_ball_1285164_640),
+    Baseball(4, R.string.baseball, R.color.sports_5, R.drawable.hit_1407826_640),
+    Basketball(5, R.string.basketball, R.color.sports_6, R.drawable.basketball_1081882_640);
 
     int value;
     int resid_str;
-    int resid_image;
+    int resid_image1;
+    int resid_image2;
 
-    SportsType(int value, int resid_str, int resid_image){
+    SportsType(int value, int resid_str, int resid_image1, int resid_image2){
         this.value = value;
         this.resid_str = resid_str;
-        this.resid_image = resid_image;
+        this.resid_image1 = resid_image1;
+        this.resid_image2 = resid_image2;
     }
 
     public int getValue(){
@@ -33,7 +42,11 @@ public enum SportsType {
         return resid_str;
     }
 
-    public int getResid_image(){
-        return resid_image;
+    public int getResid_image1(){
+        return resid_image1;
+    }
+
+    public int getResid_image2(){
+        return resid_image2;
     }
 }
--- app/src/main/java/com/iron/dragon/sportstogether/ui/adapter/MainAdapter.java
+++ app/src/main/java/com/iron/dragon/sportstogether/ui/adapter/MainAdapter.java
@@ -2,13 +2,17 @@ package com.iron.dragon.sportstogether.ui.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Point;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,19 +24,27 @@ import com.iron.dragon.sportstogether.enums.SportsType;
 import com.iron.dragon.sportstogether.factory.Sports;
 import com.iron.dragon.sportstogether.ui.activity.BulletinListActivity;
 import com.iron.dragon.sportstogether.ui.activity.LoginActivity;
+import com.iron.dragon.sportstogether.util.StringUtil;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
 
 public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder>{
     private static final String TAG = "MainAdapter";
-    private List<Sports> mDataset;
+    SportsType[] mItems;
     private Context mContext;
+    private int mScreenWidth;
 
-    public MainAdapter(Context context, List<Sports> items) {
-        mDataset = items;
-        Log.v(TAG, "mDataset="+mDataset.toString());
+    public MainAdapter(Context context) {
         mContext = context;
+        //mItem = items;
+        mItems = SportsType.values();
+        WindowManager wm = (WindowManager)mContext.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        Point p = new Point();
+        display.getSize(p);
+        mScreenWidth = p.x;
     }
 
     @Override
@@ -43,194 +55,67 @@ public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder>{
 
     @Override
     public void onBindViewHolder(ViewHolder holder, final int position) {
-        //holder.iv.setImageResource(R.drawable.a);
-        int res = 0;
-        SportsType sports = SportsType.values()[position];
-        if(sports.equals(SportsType.Badminton)){
-            res = R.drawable.badminton;
-        }else if(sports.equals(SportsType.Tennis)){
-            res = R.drawable.tennis;
-        }else if(sports.equals(SportsType.Table_tennis)){
-            res = R.drawable.table_tennis;
-        }else if(sports.equals(SportsType.Soccer)){
-            res = R.drawable.soccer;
-        }else if(sports.equals(SportsType.Baseball)){
-            res = R.drawable.baseball;
-        }else if(sports.equals(SportsType.Basketball)) {
-            res = R.drawable.basketball;
-        }else{
-            res = R.drawable.t;
-        }
-
-        holder.tv.setText(mDataset.get(position).getName());
-        holder.iv.setImageResource(res);
+        SportsType item = mItems[position];
+        int height=0;
+        if(position==1 || position==(mItems.length-1))
+            height = 400;
+        else
+            height = 800;
+        holder.tv.setText(StringUtil.getStringFromSports(mContext, item.getResid()));
         Animation animation = AnimationUtils.loadAnimation(mContext, android.R.anim.slide_in_left);
         holder.iv.setAnimation(animation);
-        final int finalRes = res;
-        final int id = (int)getItemId(position);
-        holder.iv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                boolean login = LoginPreferences.GetInstance().IsLogin(mContext.getApplicationContext(), id);
-                Log.v(TAG, "login="+login);
-
-                // if 로그인 되어있으면 게시판으로
-                // else 로그인안되어 있으면 profile edit창으로
-                Intent i = new Intent();
-                if(login){
-                    Profile profile = LoginPreferences.GetInstance().loadSharedPreferencesProfile(mContext, id);
-                    i.putExtra("MyProfile", profile);
-                    i.putExtra("Extra_Sports", id);
-                    i.putExtra("Extra_SportsImg", finalRes);
-                    i.setClass(mContext, BulletinListActivity.class);
-                    mContext.startActivity(i);
-                    //requestFriends();
-                    Toast.makeText(mContext.getApplicationContext(), "Logged in", Toast.LENGTH_SHORT).show();
-                }else{
-                    i.putExtra("Extra_Sports", id);
-                    i.setClass(mContext, LoginActivity.class);
-                    mContext.startActivity(i);
-                }
-
-            }
-        });
-    }
-
-    /*void requestFriends(){
-        final Profile myProfile = LoginPreferences.GetInstance().getLocalProfile(mContext);
-        String url = Const.MAIN_URL + "/profiles?" + "username=" + myProfile.getUsername() + "&sportsid=" + myProfile.getSportsid()
-                                                                + "&locationid=" + myProfile.getLocationid() + "&reqFriends=" + "true";
-        Log.v(TAG, "url="+url);
-        final String urlPath = Const.MAIN_URL + "/profiles";
-        StringRequest request = new StringRequest(
-                Request.Method.GET,
-                urlPath,
-                new com.android.volley.Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        println("onResponse() 호출됨 : " + response);
-                        Gson gson = new Gson();
-                        try {
-                            JSONObject obj = new JSONObject(response);
-                            String command = obj.getString("command");
-                            String code = obj.getString("code");
-                            JSONArray arr = obj.getJSONArray("message");
-                            ArrayList<Profile> al = new ArrayList<Profile>();
-                            for(int i=0; i<arr.length(); i++){
-                                Profile p = gson.fromJson(arr.get(i).toString(), Profile.class);
-                                al.add(p);
-                                Log.v(TAG, "p="+p.toString());
-                            }
-                            Intent i = new Intent(mContext, BuddyListActivity.class);
-                            HashMap<String, Object> map = new HashMap<String, Object>();
-                            map.put("data", al);
-                            i.putExtra("buddyList", map);
-                            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_SINGLE_TOP|Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                            mContext.startActivity(i);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                        //println("응답 command : " + chatData.command);
-                    }
-                },
-                new com.android.volley.Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        println("onErrorResponse() 호출됨 : " + error.getMessage());
-                    }
-                }
-        ) {
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                return super.getParams();
-            }
-
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                return super.getHeaders();
-            }
-
-            @Override
-            public String getUrl() {
-                StringBuilder sb = new StringBuilder(urlPath);
-                try {
-                    String  param1 = URLEncoder.encode(myProfile.getUsername(), "UTF-8");
-                    sb.append("?username=").append(param1)
-                            .append("&sportsid=").append(myProfile.getSportsid())
-                            .append("&locationid=").append(myProfile.getLocationid())
-                            .append("&reqFriends=").append("true");
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                }
-                Log.v(TAG, "getUrl="+sb.toString());
-                return sb.toString();
-            }
-        };
-
-        request.setShouldCache(false);
-        Volley.newRequestQueue(mContext).add(request);
-        println("웹서버에 요청함 : " + Const.MAIN_URL);
-
-        *//*
-        * GitHubService.ServiceGenerator.changeApiBaseUrl(Const.MAIN_URL);
-        GitHubService gitHubService = GitHubService.ServiceGenerator.retrofit.create(GitHubService.class);
-        final Call<JSONObject> call = gitHubService.getProfiles(username, position, 0, 1);
-        call.enqueue(new Callback<JSONObject>() {
-            @Override
-            public void onResponse(Call<JSONObject> call, Response<JSONObject> response) {
-                int code = response.code();
-                android.util.Log.d(TAG, "code = " + response.code() + " issuccessful = " + response.isSuccessful());
-                if(code == 200){
-                    Log.v(TAG, "onResponse : "+response.toString());
-                    Log.v(TAG, "onResponse : "+response.body());
-                    *//**//*JSONArray jArr = new JSONArray(response.body());
-                    Log.v(TAG, response.body().toString());
-                    Gson gson = new Gson();
-                    ArrayList<Profile> pArr = new ArrayList<Profile>();
-                    for(int i=0; i<jArr.length(); i++){
-                        Profile p = null;
-                        try {
-                            p = gson.fromJson(jArr.get(i).toString(), Profile.class);
-                            Log.v(TAG, "profile["+i+"] : "+p.toString());
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                        pArr.add(p);
-                    }*//**//*
-                }else{
-                    Log.v(TAG, response.errorBody().toString());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<JSONObject> call, Throwable t) {
-                android.util.Log.d(TAG, "error message = " + t.getMessage());
-            }
-        });*//*
-    }*/
-
-    void println(String data){
-        System.out.println(data+"\n");
+        //holder.iv.getLayoutParams().width = mScreenWidth/2;
+        //holder.iv.getLayoutParams().height = height;
+        Log.v(TAG, "item.getResid_image1()="+item.getResid_image1());
+        //holder.iv.setBackgroundResource(item.getResid_image1());
+        holder.flayout_sports.getLayoutParams().height = height;
+        holder.flayout_sports.getLayoutParams().width = mScreenWidth/2;
+        holder.flayout_sports.setBackgroundResource(item.getResid_image1());
+        //Picasso.with(mContext).load(item.getResid_image1()).resize(mScreenWidth/2, height).centerCrop().into(holder.iv);
     }
 
     @Override
     public int getItemCount() {
-        return mDataset.size();
+        return mItems.length;
     }
+
     @Override
     public long getItemId(int position) {
-        Sports sports = mDataset.get(position);
-        return sports.getId();
+        SportsType item = mItems[position];
+        return item.getValue();
     }
-    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
         ImageView iv;
         TextView tv;
+        FrameLayout flayout_sports;
 
         public ViewHolder(View view){
             super(view);
             iv = (ImageView)view.findViewById(R.id.imageView);
             tv = (TextView)view.findViewById(R.id.textView);
+            flayout_sports = (FrameLayout)view.findViewById(R.id.flayout_sports);
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    SportsType item = mItems[getAdapterPosition()];
+                    boolean login = LoginPreferences.GetInstance().IsLogin(v.getContext(), item.getValue());
+                    Log.v(TAG, "login="+login);
+
+                    Intent i = new Intent();
+                    if(login){
+                        Profile profile = LoginPreferences.GetInstance().loadSharedPreferencesProfile(v.getContext(), item.getValue());
+                        i.putExtra("MyProfile", profile);
+                        i.putExtra("Extra_Sports", item.getValue());
+                        i.putExtra("Extra_SportsImg", item.getResid_image2());
+                        i.setClass(mContext, BulletinListActivity.class);
+                    }else{
+                        i.putExtra("Extra_Sports", item.getValue());
+                        i.setClass(mContext, LoginActivity.class);
+                    }
+                    mContext.startActivity(i);
+                }
+            });
         }
     }
 }
