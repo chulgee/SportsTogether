4736080f5d3caaf9d08b660d66285ad0fe1cd81e
chulchoicezee <chulchoicezee@gmail.com>
2017-02-10 20:12:48 +0900

    add read phone permission

--- app/src/main/java/com/iron/dragon/sportstogether/SportsApplication.java
+++ app/src/main/java/com/iron/dragon/sportstogether/SportsApplication.java
@@ -1,6 +1,9 @@
 package com.iron.dragon.sportstogether;
 
 import android.app.Application;
+import android.telecom.TelecomManager;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 
 import com.iron.dragon.sportstogether.data.bean.Profile;
 import com.iron.dragon.sportstogether.util.Const;
@@ -18,7 +21,7 @@ public class SportsApplication extends Application {
     private Socket socket;
     private String regid;
     private Profile myProfile;
-
+    private static String device_id;
 
 
     private static final String TAG = "SportsApplication";
@@ -29,6 +32,13 @@ public class SportsApplication extends Application {
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
+
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
     }
 
     public Socket getSocket(){
@@ -53,5 +63,13 @@ public class SportsApplication extends Application {
 
     public void setMyProfile(Profile myProfile) {
         this.myProfile = myProfile;
+    }
+
+    public static String getDeviceID(){
+        return device_id;
+    }
+
+    public static void setDeviceID(String id){
+        device_id = id;
     }
 }
